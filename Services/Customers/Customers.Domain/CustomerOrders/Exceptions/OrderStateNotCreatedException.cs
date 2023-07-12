7cbb01f444b19077d8ee7736c798f1102bd177b2
dionismda <dionismda@gmail.com>
2023-07-12 15:44:37 -0300

    refactor: removendo alguns todos de exceptions

---
+++ Services/Customers/Customers.Domain/CustomerOrders/Exceptions/OrderStateNotCreatedException.cs
@@ -0,0 +1,7 @@
+﻿namespace Customers.Domain.CustomerOrders.Exceptions;
+
+public class OrderStateNotCreatedException : Exception
+{
+    public OrderStateNotCreatedException() : base($"The current state is not Created")
+    { }
+}
