c08a82af3b6695dfd198e6a19c6304b8c630d34d
Dionis Mendanha <dionismda@gmail.com>
2023-07-23 11:16:36 -0300

    feat: Adicionando serviço de log com serilog

---
+++ BuildingBlocks/Loggings/SerilogServices/SerilogContext.cs
@@ -0,0 +1,42 @@
+﻿namespace SerilogServices;
+
+public abstract class SerilogContext : IDisposable
+{
+    protected IDisposable? SerilogLogContext { get; private set; }
+    protected List<Tag> Tags { get; set; } = new();
+
+    private readonly Action<List<Tag>>? _action;
+
+    private volatile bool _disposedValue;
+
+    protected SerilogContext(Action<List<Tag>>? action = null)
+    {
+        _action = action;
+    }
+
+    public virtual SerilogContext CreateLog()
+    {
+        if (_action != null)
+            _action.Invoke(Tags);
+
+        SerilogLogContext = LogContext.Push(Tags.Select(it => new PropertyEnricher(it.Key, it.Value, true)).ToArray());
+
+        return this;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposedValue) return;
+
+        if (disposing)
+            SerilogLogContext?.Dispose();
+
+        _disposedValue = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ BuildingBlocks/Loggings/SerilogServices/SerilogExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SerilogServices;
+
+public static class SerilogExtensions
+{
+    public static List<Tag> AddMetadata(this List<Tag> tags, string key, string value) => tags.Add(key, value);
+
+    private static List<Tag> Add(this List<Tag> tags, string key, object value)
+    {
+        tags.Add(new Tag(key, value));
+        return tags;
+    }
+}
