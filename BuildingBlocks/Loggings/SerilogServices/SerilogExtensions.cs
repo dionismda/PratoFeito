ccc6142053890e6395333e2703de4bbf8ccfe73e
dionismda <dionismda@gmail.com>
2023-07-25 09:22:06 -0300

    build: Ajustando configurações para que a aplicação funcione iniciando pelo docker

--- BuildingBlocks/Loggings/SerilogServices/SerilogExtensions.cs
+++ BuildingBlocks/Loggings/SerilogServices/SerilogExtensions.cs
@@ -17,7 +17,7 @@ public static class SerilogExtensions
             .Enrich.FromLogContext()
             .Enrich.WithExceptionDetails()
             .Enrich.WithCorrelationId()
-            .Enrich.WithProperty("ApplicationName", $"API Serilog - {Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}")
+            .Enrich.WithProperty("PratoFeito", $"API Serilog - {Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}")
             .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.StaticFiles"))
             .Filter.ByExcluding(z => z.MessageTemplate.Text.Contains("Business error"))
             .WriteTo.Async(wt => wt.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}"))
