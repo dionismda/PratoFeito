139bd1fd1ecc02b3ef02e12ec177c3c4fac60687
dionismda <dionismda@gmail.com>
2023-08-09 15:23:29 -0300

    feat: iniciando caso de uso para editar o restaurant

---
+++ Services/Restaurants/Restaurants.Application/Restaurants/Commands/UpdateRestaurants/UpdateRestaurantCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurants.Application.Restaurants.Commands.UpdateRestaurants;
+
+public sealed class UpdateRestaurantCommand : RestaurantCommand
+{
+    public Identifier Id { get; private set; } = null!;
+
+    private UpdateRestaurantCommand() : base() { }
+
+    public UpdateRestaurantCommand(Identifier id, string name) : base(name)
+    {
+        Id = id;
+    }
+}
